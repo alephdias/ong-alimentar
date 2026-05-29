3715fd403a395961650ddd23297c9c3a5c615d07
Aleph Dias <adias@deltadecisao.com.br>
2026-05-29 12:18:39 -0300

    fix: remove next-auth do page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 export const dynamic = 'force-dynamic'
-
 import LandingPage from '@/components/landing/LandingPage'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-  if (session) redirect('/dashboard')
   return <LandingPage />
 }
